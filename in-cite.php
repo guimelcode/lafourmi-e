d6f10690f7dc6b39d6878e126495bf2fd5b60841
Guillaume GuiMel Melennec <guillaume.melennec@gmail.com>
2021-07-28 20:32:32 +0200

    put example custom template

---
+++ in-cite.php
@@ -0,0 +1,27 @@
+<?php
+/*
+    Template Name: In Cite
+*/
+
+get_header();?>
+<div id="front">
+
+	<header class="fake-header">
+		<h1>IN-CITE</h1>
+	</header>
+
+	<ul class="tab-list">
+	<?php if (have_posts()): while (have_posts()): the_post()?>
+			<li class="tab-list-item">
+				<h2><?php the_title()?></h2>
+				<?php the_post_thumbnail(); /* Trouver l'image */ ?> 
+				<p class="post-meta"> |  <?php the_time(get_option('date_format'));?></p>
+				<p>
+					<a href="<?php the_permalink();?>" class="post__link">Lire la suite</a>
+				</p>
+			</li>
+	<?php endwhile;endif;?>
+	</ul>
+</div>
+
+<?php get_footer();?>

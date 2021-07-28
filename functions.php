5f2a4993739473f1aececbabbba3844571b1ee73
Guillaume GuiMel Melennec <guillaume.melennec@gmail.com>
2021-07-28 22:48:55 +0200

    Register style and script

--- functions.php
+++ functions.php
@@ -1,7 +1,40 @@
-<?php 
+<?php
 
 // Ajouter la prise en charge des images mises en avant
-add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
-add_theme_support( 'title-tag' );
+add_theme_support('title-tag');
+
+// Enregistrement des ressources CSS et Js du thème
+function lafourmie_register_assets()
+{
+    // Ajout de jQuery
+    wp_enqueue_script('jquery');
+
+    // Ajout du JS
+    wp_enqueue_script(
+        'lafourmie',
+        get_template_directory_uri() . '/js/script.js',
+        array('jquery'),
+        '1.0',
+        true
+    );
+
+    // Déclarer style.css à la racine du thème
+    wp_enqueue_style(
+        'lafourmie-default',
+        get_stylesheet_uri(),
+        array(),
+        '1.0'
+    );
+
+    // Ajout du CSS -- issue d'un scss
+    wp_enqueue_style(
+        'lafourmie',
+        get_template_directory_uri() . '/css/main.css',
+        array(),
+        '1.0'
+    );
+}
+add_action( 'wp_enqueue_scripts', 'lafourmie_register_assets' );

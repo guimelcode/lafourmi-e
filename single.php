3bfd8e5f72024f2f9278499e0e0cd917a05dd0ed
Guillaume GuiMel Melennec <guillaume.melennec@gmail.com>
2021-08-04 14:39:16 +0200

    Construct Header of Single template

--- single.php
+++ single.php
@@ -1,20 +1,30 @@
 <?php get_header();?>
 <div id="single">
-	<header class="fake-header">
+	<!-- <header class="fake-header">
 		<h1>SINGLE</h1>
-	</header>
+	</header> -->
 	<?php if (have_posts()): while (have_posts()): the_post();?>
 		<article class="post">
-			<header class="post-header">
-				<?php the_post_thumbnail(); /* Trouver l'image */?>
-				<h2 class="post-header-title">
+			<header class="post-header segment">
+				<?php if(get_the_post_thumbnail( )): ?>
+					<div class="post-header-image">
+						<?php the_post_thumbnail(); /* Trouver l'image */?>
+					</div>
+				<?php endif; ?>
+				<h1 class="post-header-title">
 					<?php the_title()?>
-				</h2>
+				</h1>
 				<div class="post-header-meta">
 					<ul>
-						<li><?php the_date();?></li>
-						<li><?php the_category();?></li>
-						<li><?php the_tags();?></li>
+						<?php if( get_the_date() !== null): ?>
+							<li> <?php the_date();  ?> </li>
+						<?php endif; ?>
+						<?php if( get_the_category()): ?>
+							<li><?php the_category();  ?></li>
+						<?php endif; ?>
+						<?php if( get_the_tags()): ?>
+							<li><?php the_tags();  ?></li>
+						<?php endif; ?>
 					</ul>
 				</div>
 			</header>

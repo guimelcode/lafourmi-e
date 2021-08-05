272318de868b61374a056443c4c5e0db683d9b21
Guillaume GuiMel Melennec <guillaume.melennec@gmail.com>
2021-08-05 08:41:05 +0200

    Archive + interaction Table

--- archive.php
+++ archive.php
@@ -5,9 +5,10 @@
 		<h1>ARCHIVE</h1>
 	</header>
 
-	<ul class="tab-list">
+	<!-- <ul class="tab-list">
 	<?php if (have_posts()): while (have_posts()): the_post()?>
 			<li class="tab-list-item">
+				<?php var_dump(the_ID()) ?> 
 				<h2><?php the_title()?></h2>
 				<?php the_post_thumbnail(); /* Trouver l'image */ ?> 
 				<p class="post-meta"> |  <?php the_time(get_option('date_format'));?></p>
@@ -16,6 +17,30 @@
 				</p>
 			</li>
 	<?php endwhile;endif;?>
-	</ul>
+
+	</ul> -->
+	
+	<div class="archive-section segment bg-vert-25">
+
+		<table class="archive-table">
+		<?php if (have_posts()): while (have_posts()): the_post()?>
+			<tr data-id="<?php the_ID() ?>">
+				<td>
+					<a href="<?php the_permalink();?>"><?php the_title()?></a>
+				</td>
+				<td>
+					<a href="<?php the_permalink();?>"><?php the_time('d/m/Y');?></a>
+				</td>
+			</tr>
+			<?php endwhile;endif;?>
+		</table>
+		<div class="archive-img">
+		<?php if (have_posts()): while (have_posts()): the_post()?>
+			<div class="archive-img-wrap" data-id="<?php the_ID() ?>">
+			<?php the_post_thumbnail(); /* Trouver l'image */ ?> 
+			</div>	
+		<?php endwhile;endif;?>
+		</div>
+	</div>
 </div>
 <?php get_footer(); ?>
--- js/script.js
+++ js/script.js
@@ -4,9 +4,72 @@ $(document).ready(function () {
   $("body").overlayScrollbars({
     className: "os-theme-fourmi-e",
     scrollbars: { visibility: "visible" },
-  }); //test
+  });
   $(".scroll-wrap ").overlayScrollbars({
     className: "os-theme-fourmi-e",
     scrollbars: { visibility: "visible" },
-  }); //test
+  });
+  /*
+  const archiveSection = $(".archive-section");
+  const rows = $(archiveSection[0]).find(".archive-table tr");
+  const imgs = $(archiveSection[0]).find(".archive-img");
+  console.log("🚀 ~ file: script.js ~ line 20 ~ imgs", imgs);
+  let currentMousePos = { x: -1, y: -1 };
+  $.each(rows, (i, row) => {
+    $(row).mousemove(function (e) {
+      currentMousePos.x = e.pageX;
+      currentMousePos.y = e.pageY;
+    });
+    $(row).on("mouseenter", function () {
+      console.log("oi", this);
+      const ID = $(this).attr("data-id");
+      console.log("🚀 ~ file: script.js ~ line 20 ~ $ ~ ID", ID);
+      const imgFromID = imgs.find("[data-id='" + ID + "']");
+      console.log(
+        "🚀 ~ file: script.js ~ line 23 ~ $ ~ imgFromID",
+        $(imgFromID[0])
+      );
+      console.log("aaaa", $(imgFromID[0]));
+      console.log(
+        "🚀 ~ file: script.js ~ line 33 ~ $ ~ currentMousePos.y + $(imgFromID[0])[0].clientHeight",
+        currentMousePos.y,
+        $(imgFromID[0])[0].clientHeight,
+        currentMousePos.y + $(imgFromID[0])[0].clientHeight
+      );
+      $(imgFromID[0]).css({
+        display: "block",
+        top: currentMousePos.y - $(imgFromID[0])[0].clientHeight + "px",
+      });
+    });
+  });
+
+*/
+
+  const archiveBehavior = (el) => {
+    const rows = el.find(".archive-table tr");
+    const imgs = el.find(".archive-img");
+    let imgFromID;
+    let theRow;
+    $(rows).on({
+      mouseenter: function () {
+        theRow = $(this);
+        imgFromID = imgs.find("[data-id='" + theRow.attr("data-id") + "']");
+        imgFromID.fadeIn({duration: 800, ease: 'ease-in'})
+      },
+      mousemove: function (e) {
+        imgFromID.css({
+          top:
+            e.pageY -
+            imgFromID[0].offsetHeight / 2 -
+            theRow.parents(".archive-table")[0].offsetTop / 1.25 +
+            "px",
+        });
+      },
+      mouseleave: function () {
+        imgFromID.fadeOut({duration: 200, ease: 'ease-out'}).css({top: 0})
+      },
+    });
+  };
+
+  $.each($(".archive-section"), (i, el) => archiveBehavior($(el)));
 });

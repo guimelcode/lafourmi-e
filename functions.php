b18a59fb3707610b46019b0e456fcb08beb6bf33
Guillaume GuiMel Melennec <guillaume.melennec@gmail.com>
2021-10-08 13:22:53 +0200

    Ajout d'une feuille de style pour l'éditeur du backoffice.

--- functions.php
+++ functions.php
@@ -102,3 +102,9 @@ function custom_header_setup()
 }
 
 add_action('after_setup_theme', 'custom_header_setup');
+
+function add_editor_style_init() {
+    add_theme_support( 'editor-styles' );
+    add_editor_style( trailingslashit( get_template_directory_uri() ) . '/css/editor.css' );
+}
+add_action( 'init', 'add_editor_style_init' );

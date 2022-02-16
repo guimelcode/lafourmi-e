d13f4a72db9ec801b0c4a1807fa439f977f218c7
Guillaume GuiMel Melennec <guillaume.melennec@gmail.com>
2022-02-16 09:37:56 +0100

    ajout de la boucle pour contenu

--- page.php
+++ page.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
-	<h1>PAGE</h1>
+	<!-- <h1>PAGE</h1> -->
+	<section class="from-page">
+	<?php
+		wp_reset_query(); // necessary to reset query
+		while ( have_posts() ) : the_post();
+			the_content();
+		endwhile; // End of the loop.
+	?>
+	</section>
 <?php get_footer(); ?>
